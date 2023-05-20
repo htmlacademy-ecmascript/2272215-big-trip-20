b3c9980282aee68204a4ed4636b91819270d61ea
lenara k <lkrocket800@gmail.com>
2023-05-20 11:21:56 +0300

    feat: add models

--- src/presenter/app-presenter.js
+++ src/presenter/app-presenter.js
@@ -1,10 +1,16 @@
 import { TripEventsPresenter } from './trip-events-presenter';
 import { HeaderPresenter } from './header-presenter';
+import MockService from '../service/mock-service';
+import { DestinationsModel, OffersModel, PointsModel } from '../model';
 
 export class AppPresenter {
   constructor() {
     this.tripEventsPresenter = new TripEventsPresenter();
     this.headerPresenter = new HeaderPresenter();
+    this.service = new MockService();
+    this.destinationsModel = new DestinationsModel(this.service);
+    this.offersModel = new OffersModel(this.service);
+    this.pointsModel = new PointsModel(this.service);
   }
 
   init() {
